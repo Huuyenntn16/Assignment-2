fe87afa4802719d9287755f3da6aec892e60a03f
Huuyenntn16 <huyennguyen07711@gmail.com>
2025-07-02 10:35:21 +0700

    Update login_test.py

--- Auto_practice/Tests/login_test.py
+++ Auto_practice/Tests/login_test.py
@@ -1,8 +1,8 @@
 from Pages.base_page import BaseTest
 from Pages.login_page import LoginPage
 
-class TestLoginWeb(BaseTest):
-    def test_login_valid(self):
+class TestLogin(BaseTest):
+    def test_login(self):
         login_page = LoginPage(self.driver)
-        login_page.dologin()
+        login_page.login()
         
