9773022d3f1031dc5cc379d7a3e891b4314fa130
HoaND <HoaNguyen.Dev241@gmail.com>
2025-07-02 10:14:37 +0700

    .

---
+++ Auto_practice/Tests/base_test.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+class Basetest:
+ 
+  @pytest.fixture
+  def setup(self):
+    self.driver = webdriver.Chrome()
+    self.driver.maximize_window()
+    self.driver.get("https://opensource-demo.orangehrmlive.com/")
+    yield
+    self.driver.quit()
