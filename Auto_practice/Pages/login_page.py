4ddcb7031e60c06ccd04d4f7570aada183e87367
Huuyenntn16 <huyennguyen07711@gmail.com>
2025-07-02 11:26:18 +0700

    Update login_page.py

--- Auto_practice/Pages/login_page.py
+++ Auto_practice/Pages/login_page.py
@@ -3,8 +3,9 @@ from pages.base_login_page import BaseLoginPage
 class LoginPage(BaseLoginPage):
     def __init__(self, driver):
         super().__init__(driver)
-
-    def login(self, username, password):
+        
+def login(self, username="Admin", password="admin123"):
         self.enter_username(username)
         self.enter_password(password)
         self.click_login()
+   
