d9e089e6539a8a023badca0f37e2c3b3921f362f
Huuyenntn16 <huyennguyen07711@gmail.com>
2025-07-02 10:37:33 +0700

    Update login_test.py

--- Auto_practice/Tests/login_test.py
+++ Auto_practice/Tests/login_test.py
@@ -4,5 +4,5 @@ from Pages.login_page import LoginPage
 class TestLogin(BaseTest):
     def test_login(self):
         login_page = LoginPage(self.driver)
-        login_page.login()
-        
+        login_page.login("Admin", "admin123")
+        assert "dashboard" in self.driver.current_url.lower()
