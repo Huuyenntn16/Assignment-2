5a18c12968ab9384dbc0dfc81b8d19894a1671b2
Huuyenntn16 <huyennguyen07711@gmail.com>
2025-07-02 10:53:50 +0700

    Update base_page.py

--- Auto_practice/Pages/base_page.py
+++ Auto_practice/Pages/base_page.py
@@ -1,12 +1,14 @@
-import pytest
-from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-class BaseTest:
-    @pytest.fixture(autouse=True)
-    def setup(self, request):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        request.cls.driver = self.driver
-        yield
-        self.driver.quit()
+class BaseLoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def enter_username(self, username):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(By.NAME, "password").send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
