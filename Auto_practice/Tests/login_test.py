0d52d563dc049d46186a1495bb2e8437981b7700
Huyen <HuyenNguyen07711@gmail.com>
2025-07-13 18:00:13 +0700

    .

--- Auto_practice/Tests/login_test.py
+++
@@ -1,8 +0,0 @@
-from Pages.base_page import BaseTest
-from Pages.login_page import LoginPage
-
-class TestLogin(BaseTest):
-    def test_login(self):
-        login_page = LoginPage(self.driver)
-        login_page.login("Admin", "admin123")
-        assert "dashboard" in self.driver.current_url.lower()
